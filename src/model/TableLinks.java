059f5a977e6bd1b835a2f10745ab2d48e340e557
Aaron Carpenter <111708978+Aaron-Carpenter@users.noreply.github.com>
2023-10-06 16:47:27 -0400

    Added to-do list in 'information' package, finished project conversion to dynamic web project, created index page and started/finished 'Add URL' section. Created 'TableLinks' in package 'Model' it is the table which will store link information ONLY.

---
+++ src/model/TableLinks.java
@@ -0,0 +1,74 @@
+/**
+ * @author Aaron Carpenter - acarpenter5@dmacc.edu
+ * CIS175 - Fall 2023
+ * Sep 9, 2023
+ */
+
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "links")
+public class TableLinks {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "LINK")
+    private String link;
+
+    @Column(name = "TIME")
+    private String time;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public TableLinks() {
+        super();
+    }
+    
+    public TableLinks(String link, String time, String description) {
+        super();
+        this.link = link;
+        this.time = time;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
